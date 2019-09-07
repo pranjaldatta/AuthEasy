25fa3ddb8dddf505c62b3c27973cabbfed1ddba4
Pranjal  Datta <pranjaldatta99@gmail.com>
2019-09-08 01:25:48 +0530

    deleting assets

--- assets/keys.js
+++
@@ -1,8 +0,0 @@
-const clientID = "986883026348-89j1e5elvktc5asn9ukoa6g74s5b5alf.apps.googleusercontent.com"
-const clientSecret = "vyqLXu2C9Isem5ZrJj-tbp34"
-const refreshToken = "1/TijvWF_JAczs3i8nSkzPtgei8WN53VzaY5ygA1rn-gA"
-const accessToken = "ya29.Glt3B_g1LG4K06BWUG-QPUB8vmWhcL1r025bGYRj6k1P5jY-x83_p-9bTEwO67av4tNAFRspWs0zpZwYugt-xJe3i-9xqe-gkSm0-l4FvklpwQzmKh8VWf_U5eiy"
-
-module.exports.clientID = clientID
-module.exports.clientSecret = clientSecret
-module.exports.refreshToken = refreshToken
